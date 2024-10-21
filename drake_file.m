326703956ad54080d0300c927b98c1af0e1aab5e
Jacob Seipert <jseipert@asu.edu>
2024-10-21 10:41:56 -0700

    Update drake_file.m

--- drake_file.m
+++ drake_file.m
@@ -1,4 +1,4 @@
-% global key
+global key
 
 right = 'B';
 left = 'A';
@@ -7,11 +7,12 @@ brick.playTone(20, 800, 500);
 
 
 function forwardT(brick)
-    brick.MoveMotor('AB', 50);
+    brick.MoveMotor('A', 50);
+    brick.MoveMotor('B', 51);
 end
 
 function leftT(brick, left, right)
-    brick.MoveMotor(left, -50);
+    brick.MoveMotor(left, -25);
     brick.MoveMotor(right, 50);
 end
 
@@ -32,24 +33,52 @@ end
 
 InitKeyboard();
 while 1
-    pause(1);
+    pause(.25);
     switch key
-        case 'space'
-            if brick.TouchPressed(1) == 0
+        case 'w'
+            disp('checking touch value');
+            touched = brick.TouchPressed(2);
+            if touched == 0
+                disp('here')
                 forwardT(brick);
-                %% lino color testing function
+                distance = brick.UltrasonicDist(4);
+
+                % lino color testing function
+
+                % constalntly scan the distance and and if the distance is
+                % certain mark turn the robot that way 
+                
+                % could have isolated block in the middle with no external
+                % walls connected
 
 
-            elseif brick.TouchPressed(1) == 1
-
-
-                if brick.UltrasonicDist(4) > 5
+            elseif touched == 1
+                disp('Touched');
+                forwardT(brick)
+                pause(.5)
+                brick.StopMotor('AB');
+                pause(1)
+                backwardsT(brick);
+                pause(.5)
+                brick.StopMotor('AB')
+               
+                if distance > 60
+                    disp('distance met going right');
                     rightT(brick, right, left);
+                    pause(.5)
+                    brick.StopMotor('AB')
+
                 else
+                    disp('distance not met turning left');
                     leftT(brick, right, left);
+                    pause(.6);
+                    brick.StopMotor('AB')
+
                 end
 
 
             end
+        case 's'
+            brick.StopMotor('AB');    
     end
 end
