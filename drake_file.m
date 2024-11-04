9c4857a02dee14c0bf6ed4c0132f867406f52787
Lino Barraza Cordova <145868132+LinoPinguino@users.noreply.github.com>
2024-11-04 09:35:52 -0700

    Update drake_file.m

--- drake_file.m
+++ drake_file.m
@@ -161,3 +161,114 @@ while 1
             break;
     end
 end
+
+
+
+
+
+
+
+%Seperator
+
+global startLocation pickUpLocation dropOffLocation stopSignLocation
+
+startLocation = "SampleColor";
+pickUpLocation = "SampleColor";
+dropOffLocation = "SampleColor";
+stopSignLocation = "Red";
+
+brick.SetColorMode(3,4)
+color_rgb = brick.ColorRGB(3);
+
+R = color_rgb(1)
+G = color_rgb(2)
+B = color_rgb(3)
+
+hasGranny = false;
+%We keep as boolean
+
+
+function color = determineColor(R, G, B)    
+    threshold = 100;
+
+    if R >= threshold && G < threshold && B < threshold
+        color = 'Red';  
+    elseif G >= threshold && R < threshold && B < threshold
+        color = 'Green';
+    elseif B >= threshold && R < threshold && G < threshold
+        color = 'Blue';
+    elseif R >= threshold && G >= threshold && B < threshold
+        color = 'Yellow';
+    else
+        color = 'unknown';
+    end
+end
+
+
+
+function moveRobot(color)
+    switch color
+        case startLocation
+            %perform certain actions, probably nothing/ignore color;
+            %Continue movement or use it to initiate pathfinding code
+            disp("Starting Point")
+            navigateToPickUp();
+
+        case pickUpLocation
+            %give full control back to computer/break while code, we will need
+            %to do something after we pick up the granny to prevent this from
+            %shutting the robot down again and again once the robot in on the
+            %maze again with granny on
+            disp("Pick Up Point")
+
+            if ~hasGranny
+                %for pre granny pickup
+                hasGranny = true;
+                giveControl();  
+                navigateToDropOff();
+            else 
+                disp("Granny already picked Up")
+                %Return to pathfinding
+
+            end
+    
+        case dropOffLocation
+            %Give full control back to computer ONLY after having Granny ON
+            disp("Drop Off Point")
+
+            if hasGranny
+                giveControl();
+            else 
+                disp("Granny not picked up")
+                %Return to pathfinding
+
+            return;
+            
+        case stopSignLocation
+            %Pase movement for some seconds, continue going, prevent this code
+            %from triggering consecutivley to prevent the robot from staying at
+            %the stop sign forever.
+            pauseMovement()
+            moveForward()
+            %return to pathfinding
+
+        case 'unknown'
+            disp("Unknown Color")
+
+
+
+
+while color == 'unknown'
+    pause(.5);
+    color_rgb = brick.ColorRGB(3);
+    R = color_rgb(1);
+    G = color_rgb(2);
+    B = color_rgb(3);
+    color = determineColor(R, G, B);
+    navigate();
+end
+
+
+
+ 
+        
